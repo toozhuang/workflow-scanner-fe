ab006458c9ca6a3bc5261f8f5c7174bfc0993620
toozhuang <wangz.today@gmail.com>
2022-02-08 16:37:49 +0800

    config: build for env

--- src/api/asr.api.tsx
+++ src/api/asr.api.tsx
@@ -21,7 +21,8 @@ export const getASRStatus = (taskId: string) => {
 // TODO: 这里要替换一下
 export const fileDownload = (taskId: string, downloadFileName: string) => {
   const isDev = process.env.NODE_ENV === 'development';
-  const baseURL = !isDev ? 'http://172.19.223.200' : 'http://localhost';
+  const baseURL = 'http://172.19.223.200';
+  // !isDev ? 'http://172.19.223.200' : 'http://localhost';
   return axios(`${baseURL}:3230/asr-service-api/upload/generate/${taskId}`, {
     method: 'GET',
     withCredentials: true,
